8e41a0a3ff0b9715a28abfd5748e5f02f543c8b7
Christophe Boucharlat <christophe.boucharlat@surycat.com>
2018-04-06 11:55:00 +0200

    updated script for automation

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Gather latest version of discord
-version=$(curl --head "https://discordapp.com/api/download?platform=linux&format=tar.gz" |grep Location |cut -d '/' -f 6)
-
-sed -i 's/Discord\ Version\ [0-9]*\.[0-9]*\.[0-9]*$/\Discord\ Version\ '$version'/' Dockerfile
+version=$(curl --head "https://discordapp.com/api/download?platform=linux&format=tar.gz" |grep location |cut -d '/' -f 6)
+sed -i 's/\(Discord Version \)[0-9]*\.[0-9]*\.[0-9]*$/\1'$version'/' Dockerfile
+sed -i 's/\(\/linux\/\).*\(\/discord-\).*\(.tar.gz -O\)/\1'$version'\2'$version'\3/' Dockerfile
 git commit -am "Discord version $version" -S
 git tag -am "Discord version $version" $version
 git push --follow-tags
