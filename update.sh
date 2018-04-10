d8f512a3e2a78116e412ac685a5d9020d1ca5793
Christophe Boucharlat <christophe.boucharlat@gmail.com>
2018-04-10 14:43:28 +0200

    Added commit signing back

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@
 version=$(curl --head "https://discordapp.com/api/download?platform=linux&format=tar.gz" |grep location |cut -d '/' -f 6)
 sed -i 's/\(Discord Version \)[0-9]*\.[0-9]*\.[0-9]*$/\1'$version'/' Dockerfile
 sed -i 's/\(\/linux\/\).*\(\/discord-\).*\(.tar.gz -O\)/\1'$version'\2'$version'\3/' Dockerfile
-git commit -am "Discord version $version" #-S
+git commit -am "Discord version $version" -S
 git tag -am "Discord version $version" $version
 git push --follow-tags
